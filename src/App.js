f81fe8b227479e57c8f4ce610217951f97e5ccfe
Siddharth-sing <sid.d20082000@gmail.com>
2023-04-08 09:30:54 +0530

    ipfs + pinata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { uploadFileToIPFS, uploadJSONToIPFS } from './pinata';
 
 function App() {
+
+  const [assetName, setAssetName] = useState("");
+  const [imageChosen, setImageChosen] = useState("");
+  const [desc, setDesc] = useState("");
+  
+
+  function uploadJsonMetadata(s) {
+    const myJSON = {
+      name: assetName,
+      description: desc,
+      image: s,
+    };
+    const jsonString = JSON.stringify(myJSON);
+    console.log(jsonString);
+
+    document.getElementById("name").value = "";
+    document.getElementById("desc").value = "";
+    document.getElementById("image").value = "";
+
+    uploadJSONToIPFS(jsonString).then(r => console.log(r));
+
+
+  }
+
+  function uploadImage() {
+    console.log("Uploading");
+    uploadFileToIPFS(imageChosen).then(function (r) {
+      console.log("Printing = ",r);
+      uploadJsonMetadata(r.pinataURL);
+    });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: "blue" }}>IPFS-Pinata Trial</h1>
+      <form>
+        <label> Name NFT__
+          <input
+            id = "name"
+            type="text"
+            onChange={(e) => setAssetName(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Description__
+          <input
+            id = "desc" 
+            type="text"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <label>Choose an Image
+          <br />
+          <input
+            id = "image"
+            type="file"
+            onChange={(e) => {
+              setImageChosen(e.target.files[0]);
+            }}
+          />
+        </label>
+        <br />
+        <br />
+      </form>
+      <button onClick={uploadImage}>upload</button>
+
     </div>
   );
 }
---
+++ src/pinata.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+//require('dotenv').config();
+const key = "01ab83aa884e1ea26016";
+const secret = "e31498e174797008926c27fc69e6b312ae6108350610151c22cc922f6ff02420";
+const FormData = require('form-data');
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: file.name,
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
